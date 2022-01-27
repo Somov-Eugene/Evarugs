da1a0d7a3dd6db623ca024030655daf168ada6d6
Somov Eugene <somov.eugene.2012@gmail.com>
2022-01-27 21:56:57 +0300

    Added Swiper slider for section 'Reviews"

--- js/main.js
+++ js/main.js
@@ -1,1 +1,10 @@
-console.log('Load main.js');
+const reviewSlider = new Swiper('.review__slider', {
+  // Optional parameters
+  loop: true,
+  speed: 400,
+  slidesPerView: 3,
+  spaceBetween: 70,
+  centeredSlides: true,
+  slideToClickedSlide: true,
+  toggle: true,
+});
